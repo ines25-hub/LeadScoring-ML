09d5b99836e7c09a164272b659189f9fd99eed87
ines25-hub <sss387573@gmail>
2026-09-21 07:41:52 +0200

    Add prediction system

--- src/predict.py
+++ src/predict.py
@@ -43,3 +43,30 @@ def predict_conversion(visitor_data):
         "classification": classification,
         "recommendation": recommendation
     }
+
+if __name__ == "__main__":
+
+    test_visitor = {
+        "Administrative": 3,
+        "Administrative_Duration": 100,
+        "Informational": 1,
+        "Informational_Duration": 30,
+        "ProductRelated": 20,
+        "ProductRelated_Duration": 500,
+        "BounceRates": 0.01,
+        "ExitRates": 0.02,
+        "PageValues": 20,
+        "SpecialDay": 0,
+        "Month": "Nov",
+        "OperatingSystems": 2,
+        "Browser": 2,
+        "Region": 1,
+        "TrafficType": 2,
+        "VisitorType": "Returning_Visitor",
+        "Weekend": False
+    }
+
+    result = predict_conversion(test_visitor)
+
+    print("\n===== PREDICTION =====")
+    print(result)
